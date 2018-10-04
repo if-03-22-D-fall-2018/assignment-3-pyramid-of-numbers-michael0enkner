a0ca2c6269fb3dbcf68ba680911adb1dde1ffb24
michael0enkner <fmgalaxy.office@gmail.com>
2018-10-04 09:42:24 +0200

    First Version

---
+++ Pyramid_of_Numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+  char user_input[MAX_DIGITS];
+  printf("Pyramid_of_Numbers\n");
+  printf("==================\n");
+  printf("Please enter a Number: ");
+  scanf("%s",user_input);
+  
+  return 0;
+}
